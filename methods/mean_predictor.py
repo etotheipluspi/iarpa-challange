7967adc75b304465616930b022ddf6c1734b2bff
coxj1990 <joncoxaz@gmail.com>
2018-06-09 09:39:33 -0700

    move normalize function to utils

--- methods/mean_predictor.py
+++ methods/mean_predictor.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 import utils.queries as qry
+import utils.submission as sbm
 from server.database import Predictions
 from methods.random_predictor import RandomPredictor
 
@@ -23,12 +24,6 @@ class MeanPredictor:
                                 .filter(Predictions.answer_id == answer_id))
         return np.mean(preds)
 
-    def normalize(self, pred):
-        total = sum(p['value'] for p in pred)
-        if total:
-            for p in pred:
-                p['value'] /= total
-
     def predict(self, session, question_id, use_rationale=False):
         answer_ids = qry.get_answer_ids(session, question_id)
         try:
@@ -36,7 +31,7 @@ class MeanPredictor:
                      'value': self.get_mean_prediction(session, a, use_rationale)}
                     for a in answer_ids]
             if len(pred) != 1:
-                self.normalize(pred)
+                sbm.normalize_pred(pred)
         except:  # if there are no preds or this method fails submit random
             pred = RandomPredictor().predict(session, question_id)
         return pred
--- utils/submission.py
+++ utils/submission.py
@@ -6,11 +6,20 @@ def add_pred(pred, user_pred):
         pred[idx]['value'] += prob
 
 
+# Normalize by dividing by number of predictors
 def normalize(pred, n):
     if not n:
         return
     for p in pred:
         p['value'] /= n
+
+
+# Normalize to ensure sum of probs equals 1
+def normalize_pred(pred):
+    total = sum(p['value'] for p in pred)
+    if total:
+        for p in pred:
+            p['value'] /= total
 
 
 def get_pred_dict(user_preds, answer_ids):
